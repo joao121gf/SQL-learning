be02831c41d19b6a1daca67dc8a53271134ebad9
João Giovani <66459932+joao121gf@users.noreply.github.com>
2025-02-21 17:59:09 -0300

    inner join

---
+++ exercicios/011-inner-join.sql
@@ -0,0 +1,42 @@
+--BusinessEntityID, FirstName,LastNmae,EmailAdress
+SELECT TOP 10 * FROM PERSON.PERSON
+SELECT BusinessEntityId, FirstName AS 'FirstName', LastName AS 'LastName', INNER JOIN EmailAdressID
+FROM Person.Person
+
+SELECT TOP 10 * FROM PERSON.EmailAddress
+SELECT p.BusinessEntityId, p.firstname, p.lastname, pe.emailaddress
+FROM PERSON.Person as P
+INNER JOIN person.EmailAddress PE on p.BusinessEntityID = pe.BusinessEntityID
+
+
+--Queremos os nomes dos produtos e as informacoes de suas subcategorias e o preço de cada um
+
+SELECT TOP 10 * FROM Production.Product
+
+SELECT prod.Name, prodCat.Name, prod.ListPrice
+FROM Production.Product AS prod
+INNER JOIN Production.ProductSubcategory prodCat on 
+prod.ProductSubcategoryID = prodCat.ProductSubcategoryID
+
+SELECT TOP 10 * FROM Person.BusinessEntityAddress
+SELECT TOP 10 * FROM Person.Address
+
+
+SELECT TOP 10 *
+FROM Person.BusinessEntityAddress BA
+INNER JOIN Person.Address PA ON PA.AddressID = BA.AddressID
+
+
+--BUSINESSENTITYID, NAME, PHONENUMBERTYPEID, PHONENUMBER
+
+SELECT TOP 10 *
+FROM Person.PhoneNumberType
+SELECT TOP 10 *
+FROM Person.PersonPhone
+
+
+SELECT PersonPhone.BusinessEntityID, PersonNumber.Name, PersonNumber.PhoneNumberTypeID, PersonPhone.PhoneNumber
+FROM Person.PhoneNumberType AS PersonNumber
+INNER JOIN Person.PersonPhone AS PersonPhone on
+PersonPhone.PhoneNumberTypeID = PersonNumber.PhoneNumberTypeID
+
